bebb01457770836bf5ff9cacb010c3e53401e14e
雨下不由人 <46298964+Iridescent-life@users.noreply.github.com>
2024-03-13 16:46:44 +0800

    更新配置

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,9 +12,9 @@ export function GET() {
   posts.splice(12);
 
   return rss({
-    title: '潮流周刊',
-    description: '记录工程师 Tw93 的不枯燥生活',
-    site: 'https://weekly.tw93.fun/',
+    title: '碎碎念',
+    description: 'xinyu的碎碎念',
+    site: 'https://weekly-nine-sigma.vercel.app/',
     customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image>`,
     items: posts.map((item) => {
       const url = item.url;
